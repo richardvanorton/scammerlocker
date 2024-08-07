14612472f5ed440ddba1b1b645915621302fa0a2
richardvanorton <github@vanorton.org>
2024-08-08 05:08:53 +0700

    change to http for proxy

--- app/actions.ts
+++ app/actions.ts
@@ -69,7 +69,7 @@ export async function reportWebsite(captchaToken: any, formData: FormData) {
         username: 'api',
         key: process.env.MAILGUN_API_KEY || '',
         proxy: {
-            protocol: 'https',
+            protocol: 'http',
             host: '103.221.54.186', // use your proxy host here
             port: 5947, // use your proxy port here
             auth: { // may be omitted if proxy doesn't require authentication
