581b9609a4d8e36d177a25510daad62f5171f9bf
richardvanorton <github@vanorton.org>
2024-08-07 22:31:49 +0700

    check before launch

--- components/component/report-form.tsx
+++ components/component/report-form.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
